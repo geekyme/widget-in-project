a318ef2d5cf227a96706b2044cdb2c7c2c2ad18a
Shawn Lim <shawnlim89@live.com>
2018-09-09 18:38:15 +0800

    Allow widget to load its own style dependencies

--- fakeloader/src/index.js
+++ fakeloader/src/index.js
@@ -1,4 +1,22 @@
 import "./index.css";
 import App from "./App";
 
+if (typeof window !== "undefined" && window.document) {
+  fetch(`${process.env.PUBLIC_URL}/asset-manifest.json`)
+    .then(res => {
+      return res.json();
+    })
+    .then(body => {
+      var styles = document.createElement("link");
+      styles.rel = "stylesheet";
+      styles.type = "text/css";
+      styles.media = "screen";
+      styles.href = `${process.env.PUBLIC_URL}/${body["main.css"]}`;
+      document.getElementsByTagName("head")[0].appendChild(styles);
+    })
+    .catch(e => {
+      console.error("Failed to load widget styles:", e);
+    });
+}
+
 export default App;
--- nextjs-starter/pages/_document.js
+++ nextjs-starter/pages/_document.js
@@ -12,15 +12,7 @@ export default class DefaultDocument extends Document {
      **/
     return (
       <html lang={this.props.__NEXT_DATA__.props.lang || "en"}>
-        <Head>
-          <link
-            rel="stylesheet"
-            type="text/css"
-            // TODO: hardcoding a link to the widget's built css here. Wrong approach
-            // The widget script should be loading his own css!
-            href="http://localhost:5000/static/css/main.c17080f1.css"
-          />
-        </Head>
+        <Head />
         <body>
           {this.props.customValue}
           <Main />
