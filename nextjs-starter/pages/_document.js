721f84ff2be6c0e425c85ae74c969ab92a7ee881
Shawn Lim <shawnlim89@live.com>
2018-09-09 16:13:25 +0800

    Hardcoded a stylesheet link to widget's static assets

--- nextjs-starter/pages/_document.js
+++ nextjs-starter/pages/_document.js
@@ -1,18 +1,25 @@
-import Document, { Head, Main, NextScript } from 'next/document'
+import Document, { Head, Main, NextScript } from "next/document";
 
 export default class DefaultDocument extends Document {
-  static async getInitialProps (ctx) {
-    return await Document.getInitialProps(ctx)
+  static async getInitialProps(ctx) {
+    return await Document.getInitialProps(ctx);
   }
 
   render() {
     /**
-    * Here we use _document.js to add a "lang" propery to the HTML object if
-    * one is set on the page.
-    **/
+     * Here we use _document.js to add a "lang" propery to the HTML object if
+     * one is set on the page.
+     **/
     return (
-      <html lang={this.props.__NEXT_DATA__.props.lang || 'en'}>
+      <html lang={this.props.__NEXT_DATA__.props.lang || "en"}>
         <Head>
+          <link
+            rel="stylesheet"
+            type="text/css"
+            // TODO: hardcoding a link to the widget's built css here. Wrong approach
+            // The widget script should be loading his own css!
+            href="http://localhost:5000/static/css/main.c17080f1.css"
+          />
         </Head>
         <body>
           {this.props.customValue}
@@ -20,6 +27,6 @@ export default class DefaultDocument extends Document {
           <NextScript />
         </body>
       </html>
-    )
+    );
   }
 }
